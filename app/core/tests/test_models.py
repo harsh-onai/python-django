aea0d09a6f4d8cfb95c94e63fc7a507ff3770b9c
Harsh Pratyush <Harsh.Pratyush@OAKNORTH.AI>
2020-05-13 02:55:46 +0530

    Added custom user model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_success(self):
+        """Test creating a new user with email"""
+        email = 'harsh.pratyush@oaknorth.ai'
+        password = 'pass123'
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
