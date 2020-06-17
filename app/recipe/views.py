774020810ed37023581f9ff8ec7e789b81f6bf42
Harsh Pratyush <Harsh.Pratyush@OAKNORTH.AI>
2020-06-18 02:15:59 +0530

    added recipe get list

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -33,9 +33,20 @@ class IngredientViewSet(BaseViewSet):
     serializer_class = serializers.IngredientSerializer
 
 
-class RecipeViewSet(BaseViewSet):
+class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = serializers.RecipeSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         return self.queryset.filter(users=self.request.user).order_by('-id')
+
+    def perform_create(self, serializer):
+        serializer.save(users=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+
+        return serializers.RecipeSerializer
